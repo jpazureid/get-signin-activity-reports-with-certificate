96c046804e637d7cc6e14c377020702602f3a6c7
Wataru Haniyama <haniyama.wataru+work@gmail.com>
2020-10-14 09:44:39 +0900

    migrate ADAL to MSAL

--- GetSigninReportsWithCert.ps1
+++ GetSigninReportsWithCert.ps1
@@ -1,4 +1,4 @@
-Add-Type -Path ".\Tools\Microsoft.IdentityModel.Clients.ActiveDirectory\Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+Add-Type -Path "Tools\Microsoft.Identity.Client\Microsoft.Identity.Client.dll"
 
 #
 # Authorization & resource Url
@@ -11,66 +11,61 @@ $outFile = "output.json"
 $data = @()
 
 #
-# Authorization & resource Url
+# Scopes
 #
-$authUrl = "https://login.microsoftonline.com/$tenantId/" 
+$scope = "$resource/.default"
+$scopes = New-Object System.Collections.ObjectModel.Collection["string"]
+$scopes.Add($scope)
 
 #
 # Get certificate
 #
-$cert = Get-ChildItem -path cert:\CurrentUser\My | Where-Object {$_.Thumbprint -eq $thumprint}
+$cert = Get-ChildItem -path cert:\CurrentUser\My | Where-Object { $_.Thumbprint -eq $thumprint }
 
 #
 # Create AuthenticationContext for acquiring token 
 # 
-$authContext = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext $authUrl, $false
+$confidentialApp = [Microsoft.Identity.Client.ConfidentialClientApplicationBuilder]::Create($clientId).WithCertificate($cert).withTenantId($tenantId).Build()
 
-#
-# Create credential for client application 
-#
-$clientCred = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.ClientAssertionCertificate $clientID, $cert
 
-#
-# Acquire the authentication result
-#
-$authResult = $authContext.AcquireTokenAsync($resource, $clientCred).Result 
-
-if ($null -ne $authResult.AccessToken) {
-    #
-    # Compose the access token type and access token for authorization header
-    #
-    $headerParams = @{'Authorization' = "$($authResult.AccessTokenType) $($authResult.AccessToken)"}
-    $url = "$resource/v1.0/auditLogs/signIns"
-
-    Write-Output "Fetching data using Uri: $url"
-
-    Do {
-        $myReport = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $url)
-        $myReportValue = ($myReport.Content | ConvertFrom-Json).value
-
-        for ($j = 0; $j -lt $myReportValue.Count; $j++) {
-            $data += $myReportValue[$j]
-        }
-
-        #
-        # Get url from next link
-        #
-        $url = ($myReport.Content | ConvertFrom-Json).'@odata.nextLink'
-
-        #
-        # Acquire token again if it expires soon
-        #
-        $expiresOn = ($authResult.ExpiresOn).AddSeconds(-300) # minus 5 min
-        $getdate = [DateTime]::UtcNow
-
-        if ($expiresOn -lt $getdate) {
-            $authResult = $authContext.AcquireTokenAsync($resource, $clientCred).Result 
-        }
-    } while ($null -ne $url)
+function Get-AccessToken() {
+    $authResult = $confidentialApp.AcquireTokenForClient($scopes).ExecuteAsync().Result
+    if ($null -eq $authResult) {
+        Write-Host "ERROR: No Access Token"
+        exit
+    }
+    return $authResult
 }
-else {
-    Write-Host "ERROR: No Access Token"
+
+Function Get-AuthorizationHeader {
+    # acquire token if it has been expired.
+    $authResult = Get-AccessToken
+    $accessToken = $authResult.AccessToken    
+    return @{'Authorization' = "Bearer $($accessToken)" }    
 }
+
+#
+# Compose the access token type and access token for authorization header
+#
+$url = "$resource/v1.0/auditLogs/signIns"
+    
+Write-Output "Fetching data using Uri: $url"
+    
+Do {
+    $headerParams = Get-AuthorizationHeader
+    $myReport = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $url)
+    $myReportValue = ($myReport.Content | ConvertFrom-Json).value
+
+    for ($j = 0; $j -lt $myReportValue.Count; $j++) {
+        $data += $myReportValue[$j]
+    }
+
+    #
+    # Get url from next link
+    #
+    $url = ($myReport.Content | ConvertFrom-Json).'@odata.nextLink'
+
+} while ($null -ne $url)
 
 $data | ConvertTo-Json | Out-File -FilePath $outfile
 Write-Host "Sign-in log is exported to $outfile"
